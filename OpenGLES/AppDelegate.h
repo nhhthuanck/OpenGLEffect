62e2b69f2d943c9499cf76d8e93c4a83bce6dc32
Thuan <hoangthuan@hoangs-mac-mini.local>
2017-12-26 18:34:13 +0700

    Initial Commit

---
+++ OpenGLES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES
+//
+//  Created by Hoang Thuan on 12/26/17.
+//  Copyright © 2017 Hoang Thuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLES/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLES
+//
+//  Created by Hoang Thuan on 12/26/17.
+//  Copyright © 2017 Hoang Thuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
